69eaa43847a98b8667230a51a8ec5e2041384c4e
Whde <460290973@qq.com>
2019-01-15 15:55:38 +0800

    添加第三方库

--- Project/PacketTunnel/PacketTunnelProvider.swift
+++ Project/PacketTunnel/PacketTunnelProvider.swift
@@ -95,9 +95,11 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
                     let raw_dom = dom.string!
                     let index = raw_dom.index(raw_dom.startIndex, offsetBy: 1)
                     let index2 = raw_dom.index(raw_dom.startIndex, offsetBy: 2)
-                    let typeStr = raw_dom.substring(to: index)
-                    let url = raw_dom.substring(from: index2)
-                    
+                    // let typeStr = raw_dom.substring(to: index)
+                    // let url = raw_dom.substring(from: index2)
+                    let typeStr = String(raw_dom[..<index])
+                    let url = String(raw_dom[index2...])
+
                     if typeStr == "s"{
                         rule_array.append(DomainListRule.MatchCriterion.suffix(url))
                     }else if typeStr == "k"{
